ca97c6b74a41ad7c846bbe10a584ef09eb44d075
Shubham <jain229@purdue.edu>
2018-09-19 12:45:42 -0400

    token struct definitions and enums implemented

--- src/LexicalAnalyzer.hpp
+++ src/LexicalAnalyzer.hpp
@@ -1,7 +1,30 @@
 #ifndef LEXICALANALYZER_H
 #define LEXICALANALYZER_H
+
 #include <iostream>
+#include <string>
+#include <vector>
 //#include "FileHandler.hpp"
+
+/* A temperory enumerator */
+/* TODO: Fill in serious values */
+enum TYPE { 
+    doggo,
+    kitty,
+    kamehameha
+};
+
+enum DATATYPE {
+    shiba,
+    inu,
+    Alhecc
+};
+
+typedef struct token {
+    std::string identifier;
+    TYPE type;
+    DATATYPE datatype;  
+} token;
 
 #endif
 class LexicalAnalyzer{
